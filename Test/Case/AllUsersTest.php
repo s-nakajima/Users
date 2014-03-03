cf57cbabec345d50068292e35b03d361f64236e1
topaz2 <topaz2.3333@gmail.com>
2014-03-03 17:20:36 +0900

    Improved to test only plugin it self

--- Test/Case/AllUsersTest.php
+++ Test/Case/AllUsersTest.php
@@ -2,8 +2,9 @@
 class AllUsersTest extends CakeTestSuite {
 
 	public static function suite() {
-		$suite = new CakeTestSuite('All tests');
-		$suite->addTestDirectoryRecursive(TESTS . 'Case');
+		$plugin = preg_replace('/^All([\w]+)Test$/', '$1', __CLASS__);
+		$suite = new CakeTestSuite(sprintf('All %s Plugin tests', $plugin));
+		$suite->addTestDirectoryRecursive(CakePlugin::path($plugin) . 'Test' . DS . 'Case');
 		return $suite;
 	}
 }
