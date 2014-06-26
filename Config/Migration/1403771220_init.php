ccfa6af9c2aadf33998a1ca65ae59f70c3d7fdbf
topaz2 <topaz2.3333@gmail.com>
2014-06-26 18:41:20 +0900

    Added schema

---
+++ Config/Migration/1403771220_init.php
@@ -0,0 +1,118 @@
+<?php
+class Init extends CakeMigration {
+
+/**
+ * Migration description
+ *
+ * @var string
+ */
+	public $description = '';
+
+/**
+ * Actions to be performed
+ *
+ * @var array $migration
+ */
+	public $migration = array(
+		'up' => array(
+			'create_table' => array(
+				'user_attributes' => array(
+					'id' => array('type' => 'integer', 'null' => false, 'default' => null, 'length' => 11, 'key' => 'primary'),
+					'type' => array('type' => 'integer', 'null' => true),
+					'required' => array('type' => 'boolean', 'null' => true),
+					'is_each_language' => array('type' => 'boolean', 'null' => true),
+					'can_read_self' => array('type' => 'boolean', 'null' => true),
+					'can_edit_self' => array('type' => 'boolean', 'null' => true),
+					'position' => array('type' => 'text', 'null' => true, 'length' => 1073741824),
+					'created_user_id' => array('type' => 'integer', 'null' => true),
+					'created' => array('type' => 'datetime', 'null' => true),
+					'modified_user_id' => array('type' => 'integer', 'null' => true),
+					'modified' => array('type' => 'datetime', 'null' => true),
+					'indexes' => array(
+						'PRIMARY' => array('unique' => true, 'column' => 'id'),
+					),
+					'tableParameters' => array(),
+				),
+				'user_attributes_users' => array(
+					'id' => array('type' => 'integer', 'null' => false, 'default' => null, 'length' => 11, 'key' => 'primary'),
+					'user_id' => array('type' => 'integer', 'null' => false),
+					'user_attribute_id' => array('type' => 'integer', 'null' => false),
+					'value' => array('type' => 'text', 'null' => true, 'length' => 1073741824),
+					'created_user_id' => array('type' => 'integer', 'null' => true),
+					'created' => array('type' => 'datetime', 'null' => true),
+					'modified_user_id' => array('type' => 'integer', 'null' => true),
+					'modified' => array('type' => 'datetime', 'null' => true),
+					'indexes' => array(
+						'PRIMARY' => array('unique' => true, 'column' => 'id'),
+					),
+					'tableParameters' => array(),
+				),
+				'user_select_attributes' => array(
+					'id' => array('type' => 'integer', 'null' => false, 'default' => null, 'length' => 11, 'key' => 'primary'),
+					'user_attribute_id' => array('type' => 'integer', 'null' => true),
+					'created_user_id' => array('type' => 'integer', 'null' => true),
+					'created' => array('type' => 'datetime', 'null' => true),
+					'modified_user_id' => array('type' => 'integer', 'null' => true),
+					'modified' => array('type' => 'datetime', 'null' => true),
+					'indexes' => array(
+						'PRIMARY' => array('unique' => true, 'column' => 'id'),
+					),
+					'tableParameters' => array(),
+				),
+				'user_select_attributes_users' => array(
+					'id' => array('type' => 'integer', 'null' => false, 'default' => null, 'length' => 11, 'key' => 'primary'),
+					'user_id' => array('type' => 'integer', 'null' => false),
+					'user_select_attribute_id' => array('type' => 'integer', 'null' => false),
+					'value' => array('type' => 'integer', 'null' => true),
+					'created_user_id' => array('type' => 'integer', 'null' => true),
+					'created' => array('type' => 'datetime', 'null' => true),
+					'modified_user_id' => array('type' => 'integer', 'null' => true),
+					'modified' => array('type' => 'datetime', 'null' => true),
+					'indexes' => array(
+						'PRIMARY' => array('unique' => true, 'column' => 'id'),
+					),
+					'tableParameters' => array(),
+				),
+				'users' => array(
+					'id' => array('type' => 'integer', 'null' => false, 'default' => null, 'length' => 11, 'key' => 'primary'),
+					'username' => array('type' => 'string', 'null' => true, 'default' => null),
+					'password' => array('type' => 'string', 'null' => true, 'default' => null),
+					'role_id' => array('type' => 'integer', 'null' => false),
+					'created_user_id' => array('type' => 'integer', 'null' => true),
+					'created' => array('type' => 'datetime', 'null' => true),
+					'modified_user_id' => array('type' => 'integer', 'null' => true),
+					'modified' => array('type' => 'datetime', 'null' => true),
+					'indexes' => array(
+						'PRIMARY' => array('unique' => true, 'column' => 'id'),
+					),
+					'tableParameters' => array(),
+				),
+			),
+		),
+		'down' => array(
+			'drop_table' => array(
+				'user_attributes', 'user_attributes_users', 'user_select_attributes', 'user_select_attributes_users', 'users'
+			),
+		),
+	);
+
+/**
+ * Before migration callback
+ *
+ * @param string $direction, up or down direction of migration process
+ * @return boolean Should process continue
+ */
+	public function before($direction) {
+		return true;
+	}
+
+/**
+ * After migration callback
+ *
+ * @param string $direction, up or down direction of migration process
+ * @return boolean Should process continue
+ */
+	public function after($direction) {
+		return true;
+	}
+}
---
+++ Config/Schema/schema.php
@@ -0,0 +1,165 @@
+<?php 
+class UserSchema extends CakeSchema {
+
+	public function before($event = array()) {
+		return true;
+	}
+
+	public function after($event = array()) {
+	}
+
+	public $groups_users = array(
+		'id' => array('type' => 'integer', 'null' => false, 'default' => null, 'length' => 11, 'key' => 'primary'),
+		'group_id' => array('type' => 'integer', 'null' => false),
+		'user_id' => array('type' => 'integer', 'null' => false),
+		'can_read' => array('type' => 'integer', 'null' => true),
+		'can_edit' => array('type' => 'integer', 'null' => true),
+		'created_user_id' => array('type' => 'integer', 'null' => true),
+		'created' => array('type' => 'datetime', 'null' => true),
+		'modified_user_id' => array('type' => 'integer', 'null' => true),
+		'modified' => array('type' => 'datetime', 'null' => true),
+		'indexes' => array(
+			'PRIMARY' => array('unique' => true, 'column' => 'id')
+		),
+		'tableParameters' => array()
+	);
+
+	public $languages_user_attributes = array(
+		'id' => array('type' => 'integer', 'null' => false, 'default' => null, 'length' => 11, 'key' => 'primary'),
+		'user_attribute_id' => array('type' => 'integer', 'null' => false),
+		'language_id' => array('type' => 'integer', 'null' => false),
+		'name' => array('type' => 'string', 'null' => true, 'default' => null),
+		'created_user_id' => array('type' => 'integer', 'null' => true),
+		'created' => array('type' => 'datetime', 'null' => true),
+		'modified_user_id' => array('type' => 'integer', 'null' => true),
+		'modified' => array('type' => 'datetime', 'null' => true),
+		'indexes' => array(
+			'PRIMARY' => array('unique' => true, 'column' => 'id')
+		),
+		'tableParameters' => array()
+	);
+
+	public $languages_user_attributes_users = array(
+		'id' => array('type' => 'integer', 'null' => false, 'default' => null, 'length' => 11, 'key' => 'primary'),
+		'user_id' => array('type' => 'integer', 'null' => false),
+		'user_attributes_id' => array('type' => 'integer', 'null' => false),
+		'language_id' => array('type' => 'integer', 'null' => false),
+		'value' => array('type' => 'text', 'null' => true, 'length' => 1073741824),
+		'created_user_id' => array('type' => 'integer', 'null' => true),
+		'created' => array('type' => 'datetime', 'null' => true),
+		'modified_user_id' => array('type' => 'integer', 'null' => true),
+		'modified' => array('type' => 'datetime', 'null' => true),
+		'indexes' => array(
+			'PRIMARY' => array('unique' => true, 'column' => 'id')
+		),
+		'tableParameters' => array()
+	);
+
+	public $languages_user_select_attributes = array(
+		'id' => array('type' => 'integer', 'null' => false, 'default' => null, 'length' => 11, 'key' => 'primary'),
+		'user_select_attribute_id' => array('type' => 'integer', 'null' => false),
+		'language_id' => array('type' => 'integer', 'null' => false),
+		'name' => array('type' => 'string', 'null' => true, 'default' => null),
+		'created_user_id' => array('type' => 'integer', 'null' => true),
+		'created' => array('type' => 'datetime', 'null' => true),
+		'modified_user_id' => array('type' => 'integer', 'null' => true),
+		'modified' => array('type' => 'datetime', 'null' => true),
+		'indexes' => array(
+			'PRIMARY' => array('unique' => true, 'column' => 'id')
+		),
+		'tableParameters' => array()
+	);
+
+	public $roles_user_attributes = array(
+		'id' => array('type' => 'integer', 'null' => false, 'default' => null, 'length' => 11, 'key' => 'primary'),
+		'role_id' => array('type' => 'integer', 'null' => false),
+		'user_attribute_id' => array('type' => 'integer', 'null' => false),
+		'can_read' => array('type' => 'boolean', 'null' => true),
+		'can_edit' => array('type' => 'boolean', 'null' => true),
+		'created_user_id' => array('type' => 'integer', 'null' => true),
+		'created' => array('type' => 'datetime', 'null' => true),
+		'modified_user_id' => array('type' => 'integer', 'null' => true),
+		'modified' => array('type' => 'datetime', 'null' => true),
+		'indexes' => array(
+			'PRIMARY' => array('unique' => true, 'column' => 'id')
+		),
+		'tableParameters' => array()
+	);
+
+	public $user_attributes = array(
+		'id' => array('type' => 'integer', 'null' => false, 'default' => null, 'length' => 11, 'key' => 'primary'),
+		'type' => array('type' => 'integer', 'null' => true),
+		'required' => array('type' => 'boolean', 'null' => true),
+		'is_each_language' => array('type' => 'boolean', 'null' => true),
+		'can_read_self' => array('type' => 'boolean', 'null' => true),
+		'can_edit_self' => array('type' => 'boolean', 'null' => true),
+		'position' => array('type' => 'text', 'null' => true, 'length' => 1073741824),
+		'created_user_id' => array('type' => 'integer', 'null' => true),
+		'created' => array('type' => 'datetime', 'null' => true),
+		'modified_user_id' => array('type' => 'integer', 'null' => true),
+		'modified' => array('type' => 'datetime', 'null' => true),
+		'indexes' => array(
+			'PRIMARY' => array('unique' => true, 'column' => 'id')
+		),
+		'tableParameters' => array()
+	);
+
+	public $user_attributes_users = array(
+		'id' => array('type' => 'integer', 'null' => false, 'default' => null, 'length' => 11, 'key' => 'primary'),
+		'user_id' => array('type' => 'integer', 'null' => false),
+		'user_attribute_id' => array('type' => 'integer', 'null' => false),
+		'value' => array('type' => 'text', 'null' => true, 'length' => 1073741824),
+		'created_user_id' => array('type' => 'integer', 'null' => true),
+		'created' => array('type' => 'datetime', 'null' => true),
+		'modified_user_id' => array('type' => 'integer', 'null' => true),
+		'modified' => array('type' => 'datetime', 'null' => true),
+		'indexes' => array(
+			'PRIMARY' => array('unique' => true, 'column' => 'id')
+		),
+		'tableParameters' => array()
+	);
+
+	public $user_select_attributes = array(
+		'id' => array('type' => 'integer', 'null' => false, 'default' => null, 'length' => 11, 'key' => 'primary'),
+		'user_attribute_id' => array('type' => 'integer', 'null' => true),
+		'created_user_id' => array('type' => 'integer', 'null' => true),
+		'created' => array('type' => 'datetime', 'null' => true),
+		'modified_user_id' => array('type' => 'integer', 'null' => true),
+		'modified' => array('type' => 'datetime', 'null' => true),
+		'indexes' => array(
+			'PRIMARY' => array('unique' => true, 'column' => 'id')
+		),
+		'tableParameters' => array()
+	);
+
+	public $user_select_attributes_users = array(
+		'id' => array('type' => 'integer', 'null' => false, 'default' => null, 'length' => 11, 'key' => 'primary'),
+		'user_id' => array('type' => 'integer', 'null' => false),
+		'user_select_attribute_id' => array('type' => 'integer', 'null' => false),
+		'value' => array('type' => 'integer', 'null' => true),
+		'created_user_id' => array('type' => 'integer', 'null' => true),
+		'created' => array('type' => 'datetime', 'null' => true),
+		'modified_user_id' => array('type' => 'integer', 'null' => true),
+		'modified' => array('type' => 'datetime', 'null' => true),
+		'indexes' => array(
+			'PRIMARY' => array('unique' => true, 'column' => 'id')
+		),
+		'tableParameters' => array()
+	);
+
+	public $users = array(
+		'id' => array('type' => 'integer', 'null' => false, 'default' => null, 'length' => 11, 'key' => 'primary'),
+		'username' => array('type' => 'string', 'null' => true, 'default' => null),
+		'password' => array('type' => 'string', 'null' => true, 'default' => null),
+		'role_id' => array('type' => 'integer', 'null' => false),
+		'created_user_id' => array('type' => 'integer', 'null' => true),
+		'created' => array('type' => 'datetime', 'null' => true),
+		'modified_user_id' => array('type' => 'integer', 'null' => true),
+		'modified' => array('type' => 'datetime', 'null' => true),
+		'indexes' => array(
+			'PRIMARY' => array('unique' => true, 'column' => 'id')
+		),
+		'tableParameters' => array()
+	);
+
+}
