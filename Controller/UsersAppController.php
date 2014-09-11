847e863be06c62a18a1d7c95be4b1246c6b9973f
topaz2 <topaz2.3333@gmail.com>
2014-09-11 08:38:12 +0000

    Fixed phpdoc errors

--- Controller/UsersAppController.php
+++ Controller/UsersAppController.php
@@ -8,10 +8,9 @@ App::uses('AppController', 'Controller');
 /**
  * Users App Controller
  *
- * @copyright     Copyright (c) Cake Software Foundation, Inc. (http://cakefoundation.org)
- * @link          http://cakephp.org CakePHP(tm) Project
- * @since         CakePHP(tm) v 0.2.9
- * @license       http://www.opensource.org/licenses/mit-license.php MIT License
+ * @author   Jun Nishikawa <topaz2@m0n0m0n0.com>
+ * @link     http://www.netcommons.org NetCommons Project
+ * @license  http://www.netcommons.org/license.txt NetCommons License
  */
 class UsersAppController extends AppController {
 
--- Test/Case/AllUsersTest.php
+++ Test/Case/AllUsersTest.php
@@ -2,6 +2,14 @@
 /**
  * All test suite
  */
+
+/**
+ * All test suite
+ *
+ * @author   Jun Nishikawa <topaz2@m0n0m0n0.com>
+ * @link     http://www.netcommons.org NetCommons Project
+ * @license  http://www.netcommons.org/license.txt NetCommons License
+ */
 class AllUsersTest extends CakeTestSuite {
 
 /**
