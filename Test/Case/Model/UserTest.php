89c493e483714bf971f0ce7756e4f2c2be230237
topaz2 <topaz2.3333@gmail.com>
2015-03-20 10:37:22 +0900

    Fix fixture name

--- Test/Case/Model/UserTest.php
+++ Test/Case/Model/UserTest.php
@@ -22,7 +22,7 @@ class UserTest extends CakeTestCase {
 	public $fixtures = array(
 		'plugin.users.user',
 		'plugin.roles.role',
-		'plugin.roles.roles_rooms_users',
+		'plugin.rooms.roles_rooms_user',
 		'app.language',
 		'app.plugin',
 		'app.frame',
