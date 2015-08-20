a321e49214d4af239c153fe3eea6a303131bb7f5
s-nakajima <nakajimashouhei@gmail.com>
2015-08-20 12:23:13 +0900

    会員管理の開発による修正

--- Controller/Component/UserSearchComponent.php
+++ Controller/Component/UserSearchComponent.php
@@ -27,32 +27,34 @@ class UserSearchComponent extends Component {
  * @link http://book.cakephp.org/2.0/en/controllers/components.html#Component::initialize
  */
 	public function initialize(Controller $controller) {
-		//$this->controller = $controller;
-		//$this->controller->Paginator = $this->controller->Components->load('Paginator');
-		//
-		////Modelの呼び出し
-		//$this->User = ClassRegistry::init('Users.User');
-		//$this->UsersLanguage = ClassRegistry::init('Users.UsersLanguage');
+		$this->controller = $controller;
+		$this->controller->Paginator = $this->controller->Components->load('Paginator');
+
+		//Modelの呼び出し
+		$this->controller->User = ClassRegistry::init('Users.User');
+		$this->controller->UsersLanguage = ClassRegistry::init('Users.UsersLanguage');
 	}
 
 /**
  * Search user
  *
- * @param array $conditions Searchable conditions
  * @return array Return search
  */
-	public function search($fields = array(), $conditions = array()) {
-		////ユーザデータ取得
-		//$this->controller->Paginator->settings = array(
-		//	'recursive' => -1,
-		//	'fields' => $this->User->searchFields($fields),
-		//	'conditions' => $this->User->searchConditions($conditions),
-		//	'joins' => $this->User->searchJoinTables(),
-		//	'order' => array($this->User->alias . '.modified' => 'desc')
-		//);
-		//$result = $this->controller->Paginator->paginate('User');
-		//
-		//return $result;
+	public function search() {
+		$fields = array();
+		$conditions = array();
+
+		//ユーザデータ取得
+		$this->controller->Paginator->settings = array(
+			'recursive' => -1,
+			'fields' => $this->controller->User->searchFields($fields),
+			'conditions' => $this->controller->User->searchConditions($conditions),
+			'joins' => $this->controller->User->searchJoinTables(),
+			'order' => array($this->controller->User->alias . '.modified' => 'desc')
+		);
+		$results = $this->controller->Paginator->paginate('User');
+
+		return $results;
 	}
 
 }
