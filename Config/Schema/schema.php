74d0b025ef185226ff0ad410ab68731ec3054f08
s-nakajima <nakajimashouhei@gmail.com>
2015-10-30 10:33:54 +0900

    schemaファイルのクラス名変更、is_deletedを追加

--- Config/Schema/schema.php
+++ Config/Schema/schema.php
@@ -17,7 +17,7 @@
  * @SuppressWarnings(PHPMD.LongVariable)
  * @SuppressWarnings(PHPMD.TooManyFields)
  */
-class AppSchema extends CakeSchema {
+class UsersSchema extends CakeSchema {
 
 /**
  * Database connection
