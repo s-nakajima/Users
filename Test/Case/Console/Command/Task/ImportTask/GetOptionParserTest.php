70c4c7154f595b8a9531eb6882c42cba9a20cf55
s-nakajima <nakajimashouhei@gmail.com>
2016-02-20 17:38:26 +0900

    ImportTask::getOptionParser()のテスト

---
+++ Test/Case/Console/Command/Task/ImportTask/GetOptionParserTest.php
@@ -0,0 +1,52 @@
+<?php
+/**
+ * ImportTask::getOptionParser()のテスト
+ *
+ * @author Noriko Arai <arai@nii.ac.jp>
+ * @author Shohei Nakajima <nakajimashouhei@gmail.com>
+ * @link http://www.netcommons.org NetCommons Project
+ * @license http://www.netcommons.org/license.txt NetCommons License
+ * @copyright Copyright 2014, NetCommons Project
+ */
+
+App::uses('NetCommonsConsoleTestCase', 'NetCommons.TestSuite');
+
+/**
+ * ImportTask::getOptionParser()のテスト
+ *
+ * @author Shohei Nakajima <nakajimashouhei@gmail.com>
+ * @package NetCommons\Users\Test\Case\Console\Command\Task\ImportTask
+ */
+class UsersConsoleCommandTaskImportTaskGetOptionParserTest extends NetCommonsConsoleTestCase {
+
+/**
+ * Plugin name
+ *
+ * @var string
+ */
+	public $plugin = 'users';
+
+/**
+ * Shell name
+ *
+ * @var string
+ */
+	protected $_shellName = 'ImportTask';
+
+/**
+ * getOptionParser()のテスト
+ *
+ * @return void
+ */
+	public function testGetOptionParser() {
+		$shell = $this->_shellName;
+		$this->$shell = $this->loadTask($shell);
+
+		//テスト実施
+		$result = $this->$shell->getOptionParser();
+
+		//チェック
+		$this->assertEquals('ConsoleOptionParser', get_class($result));
+	}
+
+}
