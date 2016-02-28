874224610fc2454c53fbb60c0b50c094bb106cbe
s-nakajima <nakajimashouhei@gmail.com>
2016-02-28 19:46:27 +0900

    Fixutureの修正

--- Test/Fixture/UsersLanguageFixture.php
+++ Test/Fixture/UsersLanguageFixture.php
@@ -42,9 +42,9 @@ class UsersLanguageFixture extends CakeTestFixture {
  */
 	public $records = array(
 		array(
-			'id' => 1,
-			'user_id' => 1,
-			'language_id' => 2,
+			'id' => '1',
+			'user_id' => '1',
+			'language_id' => '2',
 			'name' => 'Lorem ipsum dolor sit amet',
 			'profile' => 'Lorem ipsum dolor sit amet, aliquet feugiat. Convallis morbi fringilla gravida, phasellus feugiat dapibus velit nunc, pulvinar eget sollicitudin venenatis cum nullam, vivamus ut a sed, mollitia lectus. Nulla vestibulum massa neque ut et, id hendrerit sit, feugiat in taciti enim proin nibh, tempor dignissim, rhoncus duis vestibulum nunc mattis convallis.',
 			'search_keywords' => 'Lorem ipsum dolor sit amet'
