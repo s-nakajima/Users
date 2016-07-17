6aab27b3d8847b47443b4d25593b692353ff1076
s-nakajima <nakajimashouhei@gmail.com>
2016-07-17 23:06:34 +0900

    ルーティングの見直し https://github.com/NetCommons3/NetCommons3/issues/309 https://github.com/NetCommons3/NetCommons/pull/284

---
+++ Config/routes.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Users routes configuration
+ *
+ * @author Shohei Nakajima <nakajimashouhei@gmail.com>
+ * @link http://www.netcommons.org NetCommons Project
+ * @license http://www.netcommons.org/license.txt NetCommons License
+ * @copyright Copyright 2014, NetCommons Project
+ */
+
+$params = array();
+
+Router::connect(
+	'/users/users/download/:user_id/:field_name/:size',
+	['plugin' => 'users', 'controller' => 'users', 'action' => 'download'],
+	['user_id' => '[0-9]+', 'size' => 'big|medium|small|thumb']
+);
+Router::connect(
+	'/users/users/download/:user_id/:field_name',
+	['plugin' => 'users', 'controller' => 'users', 'action' => 'download', 'size' => 'medium'],
+	['user_id' => '[0-9]+', 'size' => 'medium']
+);
+Router::connect(
+	'/users/users/:action/:user_id',
+	['plugin' => 'users', 'controller' => 'users'],
+	['user_id' => '[0-9]+']
+);
+Router::connect(
+	'/users/users/:action/:user_id',
+	['plugin' => 'users', 'controller' => 'users'],
+	['user_id' => '[0-9]+']
+);
