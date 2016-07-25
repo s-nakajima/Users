765c26704b2ca7e60f907b3b63e060358114a767
s-nakajima <nakajimashouhei@gmail.com>
2016-07-26 00:02:28 +0900

    インポートの見直し

--- Model/Behavior/ImportExportBehavior.php
+++ Model/Behavior/ImportExportBehavior.php
@@ -29,7 +29,7 @@ class ImportExportBehavior extends ModelBehavior {
 	const RANDAMSTR = '1234567890abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!#$%&=-~+*?@_';
 
 /**
- * エクスポート用のランダム文字列
+ * 最大LIMIT
  *
  * @var const
  */
@@ -46,47 +46,124 @@ class ImportExportBehavior extends ModelBehavior {
 	);
 
 /**
+ * インポートタイプ(重複エラーとする)
+ *
+ * @var const
+ */
+	const IMPORT_TYPE_NEW = '0';
+
+/**
+ * インポートタイプ(重複は上書きとする)
+ *
+ * @var const
+ */
+	const IMPORT_TYPE_UPDATE = '1';
+
+/**
+ * インポートタイプ(重複はスキップする)
+ *
+ * @var const
+ */
+	const IMPORT_TYPE_SKIP = '2';
+
+/**
  * インポート処理
- * 後で、ちゃんと仕様を考えて作る
  *
  * @param Model $model Model using this behavior
  * @param string $filePath ファイルのパス
+ * @param int $importType インポートタイプ
  * @return bool True on success, false on failure
  */
-	public function importUsers(Model $model, $filePath) {
+	public function importUsers(Model $model, $filePath, $importType = self::IMPORT_TYPE_NEW) {
 		App::uses('CsvFileReader', 'Files.Utility');
+		$this->_bindModel($model);
 
 		//$model->begin();
 		$model->prepare(true);
 
 		$reader = new CsvFileReader($filePath);
+CakeLog::debug(var_export($reader, true));
+//		$fileHeader = $this->_parseCsvHeader($model, $reader[0]);
+//		unset($reader[0]);
+
+		$reader = new CsvFileReader($filePath);
 		foreach ($reader as $i => $row) {
 			if ($i === 0) {
-				$header = $row;
+				$fileHeader = $this->_parseCsvHeader($model, $row);
 				continue;
 			}
-			$row = array_combine($header, $row);
-			$row['User.id'] = null;
-			$row['User.password_again'] = $row['User.password'];
 
-			$data = Hash::expand($row);
-			$data = Hash::insert($data, 'UsersLanguage.{n}.id');
-			$data = Hash::insert($data, 'UsersLanguage.0.language_id', '1');
-			$data = Hash::insert($data, 'UsersLanguage.1.language_id', '2');
-
+			$data = $this->_getCsvUser($model, $row, $fileHeader, $importType);
 			CakeLog::debug(var_export($data, true));
-
-			if (! $model->saveUser($data)) {
-				//バリデーションエラーの場合
-				//CakeLog::debug(var_export($data, true));
-				return false;
-			}
+//			if (! $model->saveUser($data)) {
+//				//バリデーションエラーの場合
+//				//CakeLog::debug(var_export($data, true));
+//				return false;
+//			}
 		}
 
 		//$model->commit();
 		//$model->rollback();
 
 		return true;
+	}
+
+/**
+ * CSVファイルのヘッダーの整形
+ *
+ * @param Model $model 呼び出しもとのModel
+ * @param array $fileHeader CSVファイルのヘッダー
+ * @return void
+ */
+	protected function _parseCsvHeader(Model $model, $fileHeader) {
+		$baseHeader = $this->_getCsvHeader($model);
+
+		$result = array();
+
+		foreach ($fileHeader as $i => $header) {
+			if (isset($baseHeader[$header])) {
+				$result[$i] = $header;
+			} elseif (in_array($header, $baseHeader, true)) {
+				$result[$i] = array_search($header, $baseHeader, true);
+			}
+		}
+
+		return $result;
+	}
+
+/**
+ * CSVファイルの取得
+ *
+ * @param Model $model 呼び出しもとのModel
+ * @param array $csvData CSVファイルのデータ
+ * @param array $fileHeader ファイルのヘッダー
+ * @param int $importType インポートタイプ
+ * @return void
+ */
+	protected function _getCsvUser(Model $model, $csvData, $fileHeader, $importType) {
+		$model->loadModels([
+			'UserAttribute' => 'UserAttributes.UserAttribute',
+			'UsersLanguage' => 'Users.UsersLanguage',
+		]);
+
+		$data = array();
+		foreach ($csvData as $i => $value) {
+			if (isset($fileHeader[$i])) {
+				$data = Hash::insert($data, $fileHeader[$i], $value);
+			}
+		}
+
+
+//			$row = array_combine($header, $row);
+//			$row['User.id'] = null;
+//			$row['User.password_again'] = $row['User.password'];
+//
+//			$data = Hash::expand($row);
+//			$data = Hash::insert($data, 'UsersLanguage.{n}.id');
+//			$data = Hash::insert($data, 'UsersLanguage.0.language_id', '1');
+//			$data = Hash::insert($data, 'UsersLanguage.1.language_id', '2');
+
+		return $data;
 	}
 
 /**
@@ -102,16 +179,10 @@ class ImportExportBehavior extends ModelBehavior {
 
 		$model->loadModels([
 			'UserSearch' => 'Users.UserSearch',
-			'UsersLanguage' => 'Users.UsersLanguage',
-			'UserAttribute' => 'UserAttributes.UserAttribute',
 		]);
+		$this->_bindModel($model);
 
-		$userAttributes = $model->UserAttribute->getUserAttriburesForAutoUserRegist();
-		$userAttributes = Hash::combine($userAttributes, '{n}.UserAttribute.key', '{n}');
-		foreach (self::$unexportFileds as $field) {
-			$userAttributes = Hash::remove($userAttributes, $field);
-		}
-		$header = $this->_getCsvHeader($model, $userAttributes);
+		$header = $this->_getCsvHeader($model);
 
 		$defaultConditions = $model->UserSearch->cleanSearchFields($queryParams);
 		$conditions = Hash::merge($defaultConditions, Hash::get($options, 'conditions', []));
@@ -129,7 +200,6 @@ class ImportExportBehavior extends ModelBehavior {
 			'order' => array('Role.id' => 'asc')
 		));
 
-		$this->_bindModel($model);
 		$users = $model->find('all', array(
 			'fields' => array_keys($header),
 			'recursive' => 0,
@@ -177,7 +247,7 @@ class ImportExportBehavior extends ModelBehavior {
 						'order' => ''
 					),
 				)
-			), true);
+			), false);
 		}
 	}
 
@@ -185,14 +255,20 @@ class ImportExportBehavior extends ModelBehavior {
  * Modelのバインド
  *
  * @param Model $model 呼び出しもとのModel
- * @param array $userAttributes 会員項目リスト
  * @return void
  */
-	protected function _getCsvHeader(Model $model, $userAttributes) {
+	protected function _getCsvHeader(Model $model) {
 		$model->loadModels([
+			'UserAttribute' => 'UserAttributes.UserAttribute',
 			'UsersLanguage' => 'Users.UsersLanguage',
 		]);
 		$languages = (new CurrentSystem())->getLanguages();
+
+		$userAttributes = $model->UserAttribute->getUserAttriburesForAutoUserRegist();
+		$userAttributes = Hash::combine($userAttributes, '{n}.UserAttribute.key', '{n}');
+		foreach (self::$unexportFileds as $field) {
+			$userAttributes = Hash::remove($userAttributes, $field);
+		}
 
 		$header = array();
 		foreach ($userAttributes as $attrKey => $userAttribute) {
